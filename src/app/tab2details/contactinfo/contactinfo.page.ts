bc61e556cb05741b4deb322f8e222bd90293f5b3
Charllieya <charllieya@gmail.com>
2021-01-15 13:29:04 -0600

    contactinfo page update

--- src/app/tab2details/contactinfo/contactinfo.page.ts
+++ src/app/tab2details/contactinfo/contactinfo.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+// import { Component } from '@angular/core';
+import { LocalStorageService } from "../contactinfo/services/local-storage.service";
 
 @Component({
   selector: "app-contactinfo",
@@ -6,45 +8,39 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./contactinfo.page.scss"],
 })
 export class ContactinfoPage implements OnInit {
-  userData = {
-    firstName: "",
-    lastName: "",
-    relationship: "",
+  title = "local-storage-app";
+  constructor(private localStorageService: LocalStorageService) {}
+  public firstname: string = "";
+  public lastname: string = "";
+  public age: number = null;
+  public queryKey: string;
+  public removeKey: string;
+  public queryResult: string;
+  public person = {
+    firstName: this.firstname,
+    lastName: this.lastname,
+    age: this.age,
   };
-  constructor() {
-    console.log("Constuctor..");
-    let contacts = [];
-    let userData = JSON.parse(localStorage.getItem("userData") || "[]");
-    contacts = [userData];
-    contacts.push(this.userData);
-
-    localStorage.setItem("userData", JSON.stringify(this.userData));
-    console.log(JSON.stringify(userData));
-    console.log(userData);
-
-    if (localStorage.length == 0) {
-      for (var i = 0; i < userData.length; i++) {
-        localStorage.setItem("userData", JSON.stringify(this.userData[i]));
-      }
-    }
+  public addPerson() {
+    this.person.firstName = this.firstname;
+    this.person.age = this.age;
+    this.person.lastName = this.lastname;
+    this.localStorageService.setItem(
+      this.firstname,
+      JSON.stringify(this.person)
+    );
     console.log("Local Storage length: " + localStorage.length);
+    console.log(this.person);
+  }
+  public getPerson(nameToRemove: string) {
+    this.queryResult = this.localStorageService.getItem(nameToRemove);
+  }
+  public deletePerson(nameToDelete: string) {
+    this.localStorageService.removeItem(nameToDelete);
+  }
+  public reset() {
+    this.localStorageService.clear();
   }
 
   ngOnInit() {}
-
-  saveData() {
-    // Once the save button is pressed, remove the old name that was in the localStorage..
-    // localStorage.clear();
-
-    // Set the first/last names in localStorage, based on the input
-    // localStorage.setItem(this.userData.firstName, this.userData.lastName);
-    localStorage.setItem("userData", JSON.stringify(this.userData));
-    console.log(JSON.stringify(this.userData));
-    console.log(this.userData);
-
-    // localStorage.setItem("userData", this.userData.lastName);
-    console.log("First name: " + localStorage.key(localStorage.length));
-    console.log("Last name: " + localStorage.getItem("userData"));
-    console.log("Length: " + localStorage.length);
-  }
 }
